4d796925842857879695ffc6f67c242b87602d86
Alexander Almeida <alexanderalmeida3040@gmail.com>
2022-08-25 15:54:13 -0300

    build add

--- src/server.ts
+++ src/server.ts
@@ -19,4 +19,8 @@ app.use(express.json())
 
 app.use(router)
 
+app.get('/', (req, res) => {
+    res.send('Hello world')
+  })
+
 app.listen(3333, () => console.log("========== Server is running =========="))
